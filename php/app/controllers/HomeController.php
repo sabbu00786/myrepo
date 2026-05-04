1b74a5ea6a818520cdac8938d65a76d726604f60
Saba <saba@gmail.com>
2026-05-04 15:40:22 +0530

    code push

--- php/app/controllers/HomeController.php
+++ php/app/controllers/HomeController.php
@@ -5,7 +5,7 @@ require_once '../app/core/Controller.php';
 class HomeController extends Controller {
 
     public function index() {
-        $data['title'] = "goodmorning  to evryone";
+        $data['title'] = "welcome";
         $this->view('home/index', $data);
     }
 }
